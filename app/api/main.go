165dafa86c478857a9c8f24a9f459f535ad4921f
Raiyan <raiyan.hafiz@housecall.ae>
2025-11-30 05:10:05 +0600

    application setup with context, logger and config

--- app/api/main.go
+++ app/api/main.go
@@ -3,23 +3,22 @@ package main
 import (
 	"company-api/app/api/app"
 	"company-api/foundation/logger"
+	"context"
 	"log"
 )
 
 func main() {
 	log.Println("Starting the application...")
 
-	_, err := logger.New()
+	logger, err := logger.New()
 	if err != nil {
 		log.Fatalf("Failed to initialize logger: %v", err)
 	}
 
-	_, err = app.LoadConfig()
+	cfg, err := app.LoadConfig()
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 
-
+	_ = app.New(context.Background(), *cfg, logger)
 }
-
-
