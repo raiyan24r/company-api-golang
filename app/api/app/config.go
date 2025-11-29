58cc40eeeded57951ac79dd7661d75e5945700c5
Raiyan <raiyan.hafiz@housecall.ae>
2025-11-30 05:11:11 +0600

    fix loading config

--- app/api/app/config.go
+++ app/api/app/config.go
@@ -21,7 +21,7 @@ type Server struct {
 }
 
 func LoadConfig() (*Config, error) {
-	viper.SetConfigName("company-api-config")
+	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("./config")
